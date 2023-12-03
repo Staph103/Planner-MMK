f670bd70fde48e0c95b21c6d9d6b1e3583b82fa8
MMK <staph103@gmail.com>
2023-12-03 14:35:48 -0500

    Fixed the colors to change according to time and worked on local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,34 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  // var highScores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  
+  // function task(){
+  //   var timeblock = $('#time-block');
+  //   var saved = localStorage.setItem("saved", text)
+  //   console.log(saved);
+  // }
+  // task();
+  
+  $('.saveBtn').on("click",function(){
+
+     // get nearby values of the description in jQuery
+    var text = $(this).siblings('.description').val();
+    // get the id attribute of the parent div element
+    var timeKey = $(this).parent().attr('id');
+    localStorage.setItem(timeKey, text);
+
+  });
+    
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,7 +40,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
 
 
 var currentTime;
@@ -33,33 +52,42 @@ function setTime (){
   }, 1000);
 }
 setTime();
-function colorTime(){
-  setInterval(function(){
+
+
+
 
   var milTime = dayjs().format('HH')
   console.log(milTime.toString());
 
+  $('.time-block').each(function(){
 
-  // if (milTime > ) {
-    
-  // }
+    var timeSlot = parseInt($(this).attr('id').split("hours-")[1]);
+    if (timeSlot < milTime ) {
+      
+      $(this).addClass('past')
+    }
+    else if(milTime == timeSlot){
 
-  },1000);
+      $(this).addClass('present')
+    }
+    else{
+      $(this).addClass('future')
+    }
 
-}
+
+
+  })
+
+
+
 colorTime();
 
 // if (condition) {
   
 // }
-var plannerTime = ['9','10','11','12','13','14','15','16','17']
+var plannerTime = [
+  {
 
+  }
+]
 
-// $('.savveBtn').onclick(function (Save){
-  
-//   var text = $("textarea").val()
-//   localStorage.getItem('',)
-// })
-
-// var text = $("").val()
-// console.log(text);
